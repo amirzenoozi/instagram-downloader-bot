94d38de39305de5df2bfa5a37a1495102392d9cf
Amirhossein <amirzenoozi72@gmail.com>
2021-07-13 11:37:48 +0430

    :tada: Initial Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const { Telegraf } = require('telegraf');
+require('dotenv').config();
+
+const token = process.env.BOT_TOKEN
+if (token === undefined) {
+  throw new Error('BOT_TOKEN must be provided!')
+}
+
+function sendLiveLocation (ctx) {
+  let lat = 42.0
+  let lon = 42.0
+  ctx.replyWithLocation(lat, lon, { live_period: 60 }).then((message) => {
+    const timer = setInterval(() => {
+      lat += Math.random() * 0.001
+      lon += Math.random() * 0.001
+      ctx.telegram.editMessageLiveLocation(lat, lon, message.chat.id, message.message_id).catch(() => clearInterval(timer))
+    }, 1000)
+  })
+}
+
+const bot = new Telegraf(token)
+bot.start(sendLiveLocation)
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
